fb64329cbbd2431d37af578f637d89d60239e521
lildworks <lildworks@gmail.com>
2020-02-14 13:22:57 +0100

    FEAT: Concat and compile the given source scripts

---
+++ src/functions/js/index.js
@@ -0,0 +1,27 @@
+exports.concatJS = concatJS;
+exports.minifyJS = minifyJS;
+
+
+const configs = require('../../configs.json');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const jsminify = require('gulp-js-minify');
+const rename = require('gulp-rename');
+const ts = require('gulp-typescript');
+
+/**
+ * @param {string|Array<string>} srcPath
+ * @param {string} destPath
+ * @param {string=} opt_fileName
+ * @param {Object=} opt_tsConfig
+ * @param {Object?} opt_srcOptions
+ * @param {Object?} opt_destOptions
+ */
+function concatJS(srcPath, destPath, opt_fileName, opt_tsConfig, opt_srcOptions, opt_destOptions) {
+    const outputFile = opt_fileName || configs.scriptFileName;
+    const compilerConfig = Object.assign({}, configs.tsCompilerConfig || {}, opt_tsConfig || {});
+    return gulp.src(srcPath, opt_srcOptions)
+        .pipe(ts(compilerConfig))
+        .pipe(concat(outputFile + configs.scriptFileExtension))
+        .pipe(gulp.dest(destPath, opt_destOptions));
+}
