1ac8275fb892a887d32a0332e9141282b09eaa84
lildworks <lildworks@gmail.com>
2020-02-14 13:24:23 +0100

    FEAT: Minify the given source HTML files

--- src/functions/html/index.js
+++ src/functions/html/index.js
@@ -18,3 +18,18 @@ function concatHTML(srcPath, destPath, opt_srcOptions, opt_destOptions) {
         .pipe(flatten())
         .pipe(gulp.dest(destPath, opt_destOptions));
 }
+
+/**
+ * @param {string|Array<string>} srcPath
+ * @param {string} destPath
+ * @param {Object=} opt_htmlminConfig
+ * @param {Object?} opt_srcOptions
+ * @param {Object?} opt_destOptions
+ */
+function minifyHTML(srcPath, destPath, opt_htmlminConfig, opt_srcOptions, opt_destOptions) {
+    const minifierConfig = Object.assign({}, configs.htmlMinifierConfig, opt_htmlminConfig || {});
+    return gulp.src(srcPath, opt_srcOptions)
+        .pipe(htmlmin(minifierConfig))
+        .pipe(flatten())
+        .pipe(gulp.dest(destPath, opt_destOptions));
+}
