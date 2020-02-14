0392b58516b2b3935551991c9a816bcddcfe5b30
lildworks <lildworks@gmail.com>
2020-02-14 13:25:26 +0100

    FEAT: Minify the given source PHP files

--- src/functions/php/index.js
+++ src/functions/php/index.js
@@ -25,3 +25,21 @@ function concatPHP(srcPath, destPath, opt_fileName, opt_srcOptions, opt_destOpti
         .pipe(insert.transform(buildUtils.processPHPContent))
         .pipe(gulp.dest(destPath, opt_destOptions));
 }
+
+/**
+ * @param {string|Array<string>} srcPath
+ * @param {string} destPath
+ * @param {string=} opt_fileName
+ * @param {Object=} opt_phpMinifyConfig
+ * @param {Object?} opt_srcOptions
+ * @param {Object?} opt_destOptions
+ * @returns {any} the Gulp.src stream
+ */
+function minifyPHP(srcPath, destPath, opt_fileName, opt_phpMinifyConfig, opt_srcOptions, opt_destOptions) {
+    const outputFile = opt_fileName || (configs.phpFileName + configs.minifiedSuffix);
+    const minifierConfig = Object.assign({}, configs.phpMinifierConfig || {}, opt_phpMinifyConfig || {});
+    return gulp.src(srcPath, opt_srcOptions)
+        .pipe(phpmin.phpMinify(minifierConfig))
+        .pipe(rename(outputFile + configs.phpFileExtension))
+        .pipe(gulp.dest(destPath, opt_destOptions));
+}
