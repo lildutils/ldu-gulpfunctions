83645ca547e9d93bc9ae8820085ab3b63bb67d97
lildworks <lildworks@gmail.com>
2020-02-14 13:26:57 +0100

    IMPR: Activate Functions

---
+++ src/index.js
@@ -0,0 +1,6 @@
+exports.buildFn = require('./functions/build');
+exports.cssFn = require('./functions/css');
+exports.htmlFn = require('./functions/html');
+exports.jsFn = require('./functions/js');
+exports.phpFn = require('./functions/php');
+
