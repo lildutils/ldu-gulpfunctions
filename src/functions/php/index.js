8285d58ca808bfc4923f227be6198b4753c4ae86
lildworks <lildworks@gmail.com>
2020-02-19 13:06:53 +0100

    IMPR: PHP functions with inner configs

--- src/functions/php/index.js
+++ src/functions/php/index.js
@@ -2,7 +2,6 @@ exports.concatPHP = concatPHP;
 exports.minifyPHP = minifyPHP;
 
 
-const configs = require('../../configs.json');
 const buildUtils = require('ldu-gulputils').buildUtils;
 const phpmin = require('@cedx/gulp-php-minify');
 const gulp = require('gulp');
@@ -19,9 +18,9 @@ const rename = require('gulp-rename');
  * @returns {any} the Gulp.src stream
  */
 function concatPHP(srcPath, destPath, opt_fileName, opt_srcOptions, opt_destOptions) {
-    const outputFile = opt_fileName || configs.phpFileName;
+    const outputFile = opt_fileName || 'includes';
     return gulp.src(srcPath, opt_srcOptions)
-        .pipe(concat(outputFile + configs.phpFileExtension))
+        .pipe(concat(outputFile + '.php'))
         .pipe(insert.transform(buildUtils.processPHPContent))
         .pipe(gulp.dest(destPath, opt_destOptions));
 }
@@ -36,10 +35,10 @@ function concatPHP(srcPath, destPath, opt_fileName, opt_srcOptions, opt_destOpti
  * @returns {any} the Gulp.src stream
  */
 function minifyPHP(srcPath, destPath, opt_fileName, opt_phpMinifyConfig, opt_srcOptions, opt_destOptions) {
-    const outputFile = opt_fileName || (configs.phpFileName + configs.minifiedSuffix);
-    const minifierConfig = Object.assign({}, configs.phpMinifierConfig || {}, opt_phpMinifyConfig || {});
+    const outputFile = opt_fileName || 'includes.min';
+    const minifierConfig = Object.assign({}, { silent: true }, opt_phpMinifyConfig || {});
     return gulp.src(srcPath, opt_srcOptions)
         .pipe(phpmin.phpMinify(minifierConfig))
-        .pipe(rename(outputFile + configs.phpFileExtension))
+        .pipe(rename(outputFile + '.php'))
         .pipe(gulp.dest(destPath, opt_destOptions));
 }
