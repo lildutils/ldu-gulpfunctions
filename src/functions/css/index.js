6f2f48c7ae7d7562a185da94b5429ecdba919dc2
lildworks <lildworks@gmail.com>
2020-02-19 13:06:33 +0100

    IMPR: CSS functions with inner configs

--- src/functions/css/index.js
+++ src/functions/css/index.js
@@ -2,7 +2,6 @@ exports.concatCSS = concatCSS;
 exports.minifyCSS = minifyCSS;
 
 
-const configs = require('../../configs.json');
 const gulp = require('gulp');
 const concat = require('gulp-concat');
 const cssmin = require('gulp-clean-css');
@@ -19,10 +18,10 @@ const sass = require('gulp-sass');
  * @returns {any} the Gulp.src stream
  */
 function concatCSS(srcPath, destPath, opt_fileName, opt_sassConfig, opt_srcOptions, opt_destOptions) {
-    const outputFile = opt_fileName || configs.styleFileName;
-    const compilerConfig = Object.assign({}, configs.sassCompilerConfig, opt_sassConfig || {});
+    const outputFile = opt_fileName || 'styles';
+    const compilerConfig = Object.assign({}, {}, opt_sassConfig || {});
     return gulp.src(srcPath, opt_srcOptions)
-        .pipe(concat(outputFile + configs.styleFileExtension))
+        .pipe(concat(outputFile + '.css'))
         .pipe(sass(compilerConfig).on('error', sass.logError))
         .pipe(gulp.dest(destPath, opt_destOptions));
 }
@@ -36,10 +35,10 @@ function concatCSS(srcPath, destPath, opt_fileName, opt_sassConfig, opt_srcOptio
  * @param {Object?} opt_destOptions
  */
 function minifyCSS(srcPath, destPath, opt_fileName, opt_cssminConfig, opt_srcOptions, opt_destOptions) {
-    const outputFile = opt_fileName || (configs.styleFileName + configs.minifiedSuffix);
-    const minifierConfig = Object.assign({}, configs.cssMinifierConfig, opt_cssminConfig || {});
+    const outputFile = opt_fileName || 'styles.min';
+    const minifierConfig = Object.assign({}, { compatibility: 'ie8' }, opt_cssminConfig || {});
     return gulp.src(srcPath, opt_srcOptions)
         .pipe(cssmin(minifierConfig))
-        .pipe(rename(outputFile + configs.styleFileExtension))
+        .pipe(rename(outputFile + '.css'))
         .pipe(gulp.dest(destPath, opt_destOptions));
 }
